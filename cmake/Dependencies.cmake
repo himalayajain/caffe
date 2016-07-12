a6303865648639c909c7d26688f94588ab473ae5
Philipp Krähenbühl <philkr@users.noreply.github.com>
2016-07-12 08:58:09 -0700

    Adding OpenMP support and speeding up convert_imageset [currently unordered, but 20% faster]

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -170,3 +170,13 @@ endif()
 if(BUILD_docs)
   find_package(Doxygen)
 endif()
+
+
+# ---[ OpenMP
+if(USE_OPENMP)
+  find_package(OpenMP)
+  if(OpenMP_CXX_FLAGS)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+  endif()
+endif()
